ae68653dbe036965e4bc586e4151d6286f78294b
chaplygindenys <chaplygindenys@gmail.com>
2022-07-02 13:11:10 +0300

    is is done with  Apollo-> testing

--- ts-file/users/resolvers/users.resolver.ts
+++ ts-file/users/resolvers/users.resolver.ts
@@ -1,16 +1,11 @@
 import console from 'console';
-import { BodyUser, User } from '../../interface.js';
-// import { TracksService } from '../services/track.service.js';
-// import { GenresService } from '../../genres/services/genres.services.js';
-// import { ArtistsService } from '../../artists/services/artists.service.js';
-// import { BandsService } from '../../bands/services/bands.service.js';
+import { User } from '../../interface.js';
 export const resolverUsers = {
   Query: {
     jwt: async (
       _source: any,
       { password, email }: any,
-      { dataSources }: any,
-      context: any
+      { dataSources }: any
     ) => {
       console.log(password, email);
       console.log(dataSources);
@@ -19,54 +14,31 @@ export const resolverUsers = {
         const body = await dataSources.usersAPI.loginUser(password, email);
         console.log(body.jwt);
         return {
-          code: 200,
-          success: true,
-          message: 'User successfully created!',
           JWT: body.jwt,
         };
       } catch (err: Error | undefined | any) {
         if (err) {
           console.error(err);
-          return {
-            code: 400,
-            success: false,
-            message: err.message,
-          };
         }
       }
     },
-    user: async (
-      _source: any,
-      { id }: any,
-      { dataSources }: any,
-      context: any
-    ) => {
+    user: async (_source: any, { id }: any, { dataSources }: any) => {
       console.log(id);
       console.log(dataSources);
-      // console.log(context);
+
       try {
         const body: User = await dataSources.usersAPI.getUser(id);
         console.log(`resolver`, body);
         return {
-          code: 200,
-          success: true,
-          message: 'User successfully created!',
-          user: {
-            id: body._id,
-            firstName: body.firstName,
-            lastName: body.lastName,
-            password: body.password,
-            email: body.email,
-          },
+          id: body._id,
+          firstName: body.firstName,
+          lastName: body.lastName,
+          password: body.password,
+          email: body.email,
         };
       } catch (err: Error | undefined | any) {
         if (err) {
           console.log(err);
-          return {
-            code: 400,
-            success: false,
-            message: err.message,
-          };
         }
       }
     },
@@ -76,12 +48,10 @@ export const resolverUsers = {
     register: async (
       _source: any,
       { firstName, lastName, password, email }: any,
-      { dataSources }: any,
-      context: any
+      { dataSources }: any
     ) => {
       console.log(firstName, lastName, password, email);
       console.log(dataSources);
-      // console.log(context);
       try {
         const body: User = await dataSources.usersAPI.postUser(
           firstName,
@@ -91,24 +61,15 @@ export const resolverUsers = {
         );
         console.log(`resolver`, body);
         return {
-          code: 201,
-          success: true,
-          message: 'User successfully created!',
-          user: {
-            id: body._id,
-            firstName: body.firstName,
-            lastName: body.lastName,
-            password: body.password,
-            email: body.email,
-          },
+          id: body._id,
+          firstName: body.firstName,
+          lastName: body.lastName,
+          password: body.password,
+          email: body.email,
         };
       } catch (err: Error | undefined | any) {
         if (err) {
-          return {
-            code: 400,
-            success: false,
-            message: err.message,
-          };
+          console.log(err);
         }
       }
     },
